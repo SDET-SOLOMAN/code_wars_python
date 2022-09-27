4d1557403084504f5a0e4022d8c929fcac3cceb1
EL CODER <sdetsoloman@gmail.com>
2022-09-26 20:26:54 -0500

    solving cata's from codewars

--- practice.py
+++ practice.py
@@ -1,10 +1,14 @@
-def count_sevens(*args):
-    return args.count(7)
+def count_photos(road):
+    photos = 0
+    left = 0
+    dot_founds = 0
+    for item in road:
+        if item == ">":
+            left += 1
+        elif item == ".":
+            photos += left
+            dot_founds += 1
+        elif item == "<":
+            photos += dot_founds
 
-
-nums = [90, 1, 35, 67, 89, 20, 3, 1, 2, 3, 4, 5, 6, 9, 34, 46, 57, 68, 79, 12, 23, 34, 55, 1, 90, 54, 34, 76, 8, 23, 34,
-        45, 56, 67, 78, 12, 23, 34, 45, 56, 67, 768, 23, 4, 5, 6, 7, 8, 9, 12, 34, 14, 15, 16, 17, 11, 7, 11, 8, 4, 6,
-        2, 5, 8, 7, 10, 12, 13, 14, 15, 7, 8, 7, 7, 345, 23, 34, 45, 56, 67, 1, 7, 3, 6, 7, 2, 3, 4, 5, 6, 7, 8, 9, 8,
-        7, 6, 5, 4, 2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 9, 8, 7, 8, 7, 6, 5, 4, 3, 2, 1, 7]
-
-print(count_sevens(*nums))
+    return photos
